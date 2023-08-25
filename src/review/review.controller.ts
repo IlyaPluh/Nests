b6ddaa9403811f74c212328fc43b170c460d44d2
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-08-25 21:11:35 +0300

    add auth

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -11,33 +11,49 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { CreateReviewDto } from './dto/create-review.dto';
-import { ReviewService } from './review.service';
-import { REVIEW_NOT_FOUND, REVIEW_DEL } from './review.constants';
+import { IdValidationPipe } from 'src/pipes/ad-validation.pipe';
+import { TelegramService } from 'src/telegram/telegram.service';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { REVIEW_NOT_FOUND } from './review.constants';
+import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviwService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly telegramService: TelegramService
+  ) {}
 
   @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
-    return this.reviwService.create(dto);
+    return this.reviewService.create(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('notify')
+  async notify(@Body() dto: CreateReviewDto) {
+    const message =
+      `Имя: ${dto.name}\n` +
+      `Заголовок: ${dto.title}\n` +
+      `Описание: ${dto.description}\n` +
+      `Рейтинг: ${dto.rating}\n` +
+      `ID Продукта: ${dto.productId}`;
+    return this.telegramService.sendMessage(message);
   }
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  async delete(@Param('id') id: string) {
-    const deletedDoc = await this.reviwService.delete(id);
+  async delete(@Param('id', IdValidationPipe) id: string) {
+    const deletedDoc = await this.reviewService.delete(id);
     if (!deletedDoc) {
       throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
-    return REVIEW_DEL;
   }
 
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {
-    return this.reviwService.findByProductId(productId);
+  async getByProduct(@Param('productId', IdValidationPipe) productId: string) {
+    return this.reviewService.findByProductId(productId);
   }
 }
